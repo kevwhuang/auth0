c7254acfd25e3fe3b81afe86ee6888615f523d29
Kevin Huang <kevwhuang@gmail.com>
2023-04-20 03:23:28 -0500

    Added 404 page

--- src/app.mjs
+++ src/app.mjs
@@ -4,12 +4,15 @@ import path from 'path';
 import url from 'url';
 
 const PATH = path.join(path.dirname(url.fileURLToPath(import.meta.url)), '../public');
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 8888;
 const app = express();
 
 app.listen(PORT);
+
 app.use(express.json());
 app.use(express.raw());
 app.use(express.text());
 app.use(express.static(PATH));
 app.use(express.urlencoded({ extended: true }));
+
+app.all('*', (req, res) => res.status(404).sendFile(path.join(PATH, '/404.html')));
